ce22f1e59e62e6207865875eb2a1c0b61a4e17cf
lumanya <shaibulumanya@gmail.com>
2025-01-05 20:56:07 +0300

    update lint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,16 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\r');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Handle input from stdin
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\r`);
-  
-  // Check if the input is piped (not interactive)
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\r');
-  }
-
-  process.exit();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
